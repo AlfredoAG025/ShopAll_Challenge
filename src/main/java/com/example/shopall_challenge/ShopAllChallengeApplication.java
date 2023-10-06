fcb8d496d915752f8a9d0d6af64319e7d765b4c8
Freddy024 <arroyo.alfredo.1pm@gmail.com>
2023-10-05 19:59:00 -0600

    base proyect

---
+++ src/main/java/com/example/shopall_challenge/ShopAllChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopall_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAllChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAllChallengeApplication.class, args);
+    }
+
+}
