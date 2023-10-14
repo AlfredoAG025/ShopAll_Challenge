c7eb055428f1d04e6074581f504762a621b2f7bd
CHIN0666 <128001410+CHIN0666@users.noreply.github.com>
2023-10-13 20:36:46 -0600

    commit_review_with_methods

--- src/main/java/com/example/shopall_challenge/controller/ReviewController.java
+++ src/main/java/com/example/shopall_challenge/controller/ReviewController.java
@@ -1,7 +1,9 @@
 package com.example.shopall_challenge.controller;
 
 import com.example.shopall_challenge.domain.GenericResponse;
+import com.example.shopall_challenge.model.Resena;
 import com.example.shopall_challenge.model.Usuario;
+import com.example.shopall_challenge.model.Vendedor;
 import com.example.shopall_challenge.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +12,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/review")
 public class ReviewController {
 
-    private final ReviewService reviewService;
-
+    private final ReviewService service;
     @Autowired
-    public ReviewController(ReviewService reviewService){
-        this.reviewService = reviewService;
+    public ReviewController(ReviewService service){
+        this.service = service;
     }
 
     @GetMapping
     public GenericResponse getReview(){
-        return this.reviewService.getReview();
+        return this.service.getReview();
     }
+
+    @GetMapping("{comentario_id}")
+    public GenericResponse getReviewById(@PathVariable("comentario_id") Long comentario_id){
+        return this.service.getReview(comentario_id);
+    }
+
+    @PostMapping("/add")
+    public GenericResponse addReview(@RequestBody Resena body){
+        return this.service.addReview(body);
+    }
+
+    @PutMapping("/update/{comentario_id}")
+    public GenericResponse updateReview(@PathVariable("comentario_id") Long comentario_id, @RequestBody Resena body){
+        return this.service.updateReview(comentario_id, body);
+    }
+
+    @DeleteMapping("/delete/{comentario_id}")
+    public GenericResponse deleteReview(@PathVariable("comentario_id") Long comentario_id){
+        return this.service.deleteReview(comentario_id);
+    }
+
+
 
 
 }
--- src/main/java/com/example/shopall_challenge/service/ReviewService.java
+++ src/main/java/com/example/shopall_challenge/service/ReviewService.java
@@ -5,22 +5,93 @@ import com.example.shopall_challenge.model.Resena;
 import com.example.shopall_challenge.model.Vendedor;
 import com.example.shopall_challenge.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService {
 
 
-    private ReviewRepository resenaRepository;
+    private ReviewRepository repository;
     public ReviewService(ReviewRepository resenaRepository) {
-        this.resenaRepository = resenaRepository;
+        this.repository = resenaRepository;
 
     }
     public GenericResponse getReview(){
-        List<Resena> sellers = this.resenaRepository.findAll();
+        List<Resena> sellers = this.repository.findAll();
         GenericResponse response =  new GenericResponse(200, "Correct", sellers);
         return response;
     }
+
+    public GenericResponse getReview(@PathVariable Long comentario_id){
+        List<Resena> reviews = new ArrayList<>();
+        Resena review;
+        Optional<Resena> review_opt = repository.findById(comentario_id);
+        GenericResponse response = null;
+
+        if (review_opt.isPresent()){
+            review = review_opt.get();
+            reviews.add(review);
+            response =  new GenericResponse(201, "Review found", reviews);
+        } else {
+            response =  new GenericResponse(409, "Review not found", reviews);
+        }
+
+        return response;
+    }
+
+    public GenericResponse addReview(@RequestBody Resena body){
+        List<Resena> reviews = new ArrayList<>();
+        reviews.add(body);
+
+        GenericResponse response;
+        try{
+            repository.save(body);
+            response =  new GenericResponse(201, "Accepted", reviews);
+        } catch (Exception e){
+            response =  new GenericResponse(409, "Conflict", reviews);
+        }
+        return response;
+    }
+
+    public GenericResponse updateReview(@PathVariable Long comentario_id, @RequestBody Resena body){
+        List<Resena> reviews = new ArrayList<>();
+        Resena review;
+        Optional<Resena> review_opt = repository.findById(comentario_id);
+        GenericResponse response = null;
+
+        if (review_opt.isPresent()){
+            review = body;
+            reviews.add(review);
+            repository.save(review);
+            response =  new GenericResponse(201, "Reviews Updated!", reviews);
+        } else {
+            response =  new GenericResponse(409, "Reviews not found", reviews);
+        }
+        return response;
+    }
+
+    public GenericResponse deleteReview(@PathVariable Long comentario_id){
+        List<Resena> sellers = new ArrayList<>();
+        Resena review;
+        Optional<Resena> review_opt = repository.findById(comentario_id);
+        GenericResponse response = null;
+
+        if (review_opt.isPresent()){
+            review = review_opt.get();
+            sellers.add(review);
+            repository.deleteById(comentario_id);
+            response =  new GenericResponse(201, "Seller with id: " + comentario_id + " deleted", sellers);
+        } else {
+            response =  new GenericResponse(409, "Seller not found", sellers);
+        }
+
+        return response;
+    }
+
 }
