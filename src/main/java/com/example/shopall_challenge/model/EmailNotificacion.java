036baa88c05a0d481a10f777074785726e8c4644
vZanahoria <20030853@itcelaya.edu.mx>
2023-10-14 12:13:42 -0600

    model EmailNotificacion

---
+++ src/main/java/com/example/shopall_challenge/model/EmailNotificacion.java
@@ -0,0 +1,24 @@
+package com.example.shopall_challenge.model;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
+@Getter
+@Setter
+public class EmailNotificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificacion_id;
+    @ManyToOne
+    @JoinColumn(name="usuario_id")
+    private Usuario usuario;
+    private String tipo_notificacion;
+    private String mensaje;
+    private LocalDate fecha_envio;
+}
