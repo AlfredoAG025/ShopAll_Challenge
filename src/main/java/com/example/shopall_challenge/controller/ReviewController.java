98890b81a77941834de783700eb759adf92735e8
CHIN0666 <128001410+CHIN0666@users.noreply.github.com>
2023-10-13 19:17:47 -0600

    starting_configuration

--- src/main/java/com/example/shopall_challenge/controller/ReviewController.java
+++ src/main/java/com/example/shopall_challenge/controller/ReviewController.java
@@ -1,11 +1,10 @@
 package com.example.shopall_challenge.controller;
 
 import com.example.shopall_challenge.domain.GenericResponse;
+import com.example.shopall_challenge.model.Usuario;
 import com.example.shopall_challenge.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/review")
@@ -14,11 +13,16 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @Autowired
-    public ReviewController(ReviewService reviewService){ this.reviewService = reviewService;
+    public ReviewController(ReviewService reviewService){
+        this.reviewService = reviewService;
     }
 
     @GetMapping
     public GenericResponse getReview(){
         return this.reviewService.getReview();
     }
+
+
 }
+
+
--- src/main/java/com/example/shopall_challenge/service/ReviewService.java
+++ src/main/java/com/example/shopall_challenge/service/ReviewService.java
@@ -4,10 +4,12 @@ import com.example.shopall_challenge.domain.GenericResponse;
 import com.example.shopall_challenge.model.Resena;
 import com.example.shopall_challenge.model.Vendedor;
 import com.example.shopall_challenge.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
+@Service
 public class ReviewService {
 
 
