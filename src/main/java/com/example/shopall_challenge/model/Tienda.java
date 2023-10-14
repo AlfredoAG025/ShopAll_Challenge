521283bbae0a765ebdcb5e3bd29a4aaf6774153d
vZanahoria <20030853@itcelaya.edu.mx>
2023-10-14 12:10:56 -0600

    Tienda FULL funcionando

---
+++ src/main/java/com/example/shopall_challenge/model/Tienda.java
@@ -0,0 +1,21 @@
+package com.example.shopall_challenge.model;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Entity
+@Table
+@Data
+@Getter
+@Setter
+public class Tienda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tienda_id;
+    @ManyToOne
+    @JoinColumn(name="vendedor_id")
+    private Vendedor vendedor;
+    private String nombre_tienda;
+    private String descripcion;
+}
